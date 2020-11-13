85cb97f63f958f265dccd828fcbfd8b2bd2438ed
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2020-11-13 14:19:13 +0530

    alert box

--- Vinjs.js
+++ Vinjs.js
@@ -2,3 +2,6 @@ console.log("Vinothkumar Js");
 function vinoth(){
     document.getElementById('ext').innerHTML = "From external js"
 }
+function myAlert() {
+    alert("Alerted from external js");
+}
