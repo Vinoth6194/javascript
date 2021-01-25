2983a25d6e82db7fa254733a27aa298a4cf17f46
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-25 11:00:25 +0530

    changed string to int for the case

--- DateSwitcher/daySwitcher.js
+++ DateSwitcher/daySwitcher.js
@@ -3,26 +3,29 @@ function myDaySwitcher(){
     var day, today;
     day = document.getElementById("day").value;
     console.log(day);
-    switch (day) {
-        case "0":
+    console.log(typeof day);
+    console.log(parseInt(day));
+    console.log(typeof day);
+    switch (parseInt(day)) {
+        case 0:
         today = "Sunday";
         break;
-        case "1":
+        case 1:
             today = "Monday";
             break;
-        case "2":
+        case 2:
             today = "Tuesday";
             break;
-        case "3":
+        case 3:
             today = "Wednesday";
             break;
-        case "4":
+        case 4:
             today = "Thursday";
             break;
-        case "5":
+        case 5:
             today = "Friday";
             break;
-        case "6":
+        case 6:
             today = "Saturday";
             break;
         default:
